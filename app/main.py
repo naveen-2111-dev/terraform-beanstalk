5ff7a85282fdfab03c3561d702c0a0ba930ae318
Rajan-5996 <rajan.naveen@gwcdata.ai>
2026-04-28 15:20:33 +0530

    Update comment for default route to clarify server status check

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 app.include_router(router)
 
-#default route
+#default route to test if the server is running
 @app.get("/")
 def root():
     return {"message": "FastAPI + DynamoDB + uv working"}
